b2c0eebc9cb2fdff6a7ff501f8fdc19c65dfba41
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-01 22:42:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from binascii import (
 )
 from pyrogram import (
     Client,
-    filters, enums
+    filters
 )
 
 from enum import Enum
@@ -120,7 +120,8 @@ async def start(bot: Client, cmd: Message):
 @Bot.on_message((filters.document | filters.video | filters.audio) & ~filters.chat(Config.DB_CHANNEL))
 async def main(bot: Client, message: Message):
 
-    if message.chat.type == enums.ChatType.PRIVATE:
+    #if message.chat.type == enums.ChatType.PRIVATE:
+    if message.chat.type == "private":
 
         await add_user_to_database(bot, message)
 
@@ -146,7 +147,8 @@ async def main(bot: Client, message: Message):
             quote=True,
             disable_web_page_preview=True
         )
-    elif message.chat.type == enums.ChatType.CHANNEL:
+    #elif message.chat.type == enums.ChatType.CHANNEL:
+    elif message.chat.type == "channel":
         if (message.chat.id == int(Config.LOG_CHANNEL)) or (message.chat.id == int(Config.UPDATES_CHANNEL)) or message.forward_from_chat or message.forward_from:
             return
         elif int(message.chat.id) in Config.BANNED_CHAT_IDS:
