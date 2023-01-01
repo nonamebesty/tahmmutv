56c47089252ac9bfc6f7b4a8fba3c26bad22011e
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-02 00:02:06 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config(object):
 	BOT_USERNAME = "filestoreasu4bot"
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001188070894"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "880087645"))
-	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://video:video@rishi.fzpls33.mongodb.net/?retryWrites=true&w=majority")
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Erichdaniken:Erichdaniken@cluster0.vhu3d.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "AsuranMoviefinder")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL",  "-1001188070894")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
