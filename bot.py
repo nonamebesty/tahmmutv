7d4f1fb764c1665f8ca21f30f9b539d73e0cf0c9
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-01 19:23:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,8 @@ from binascii import (
 )
 from pyrogram import (
     Client,
+    enums,
     filters
-    #,enums
 )
 from pyrogram.errors import (
     UserNotParticipant,
@@ -41,8 +41,8 @@ from handlers.save_media import (
 MediaList = {}
 
 Bot = Client(
-    Config.BOT_USERNAME,
-    #in_memory=True,
+    name=Config.BOT_USERNAME,
+    in_memory=True,
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH
