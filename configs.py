c0af8a0783b2f4dd391aaeecba06b9a4c8f43ed9
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-01 12:36:31 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,7 +7,7 @@ class Config(object):
 	API_ID = 1923471
 	API_HASH = "fcdc178451cd234e63faefd38895c991"
 	BOT_TOKEN = "5670135699:AAG40R23a7vhHXYmDlGDYNPe-N2xDACD8QM"
-	BOT_USERNAME = "ppvideostoressbot"
+	BOT_USERNAME = "filestoreasu4bot"
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001188070894"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "880087645"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://video:video@rishi.fzpls33.mongodb.net/?retryWrites=true&w=majority")
@@ -15,7 +15,7 @@ class Config(object):
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL",  "-1001188070894")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
