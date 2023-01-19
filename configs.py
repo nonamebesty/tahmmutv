100c871d463fe0161e8cd07a553e62101dc28b05
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-19 22:01:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -22,7 +22,7 @@ class Config(object):
 This is Permanent Files Store Bot!
 Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
 
-🤖 **My Name:** [Files Store Bot](https://t.me/{FileStoreOfficialBot})
+🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
 
 📝 **Language:** [Python3](https://www.python.org)
 
