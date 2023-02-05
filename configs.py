96dbf1325112c3827a3cc71f3e0c2dca9e50cc72
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-02-05 22:01:43 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,8 +6,8 @@ import os
 class Config(object):
 	API_ID = 1923471
 	API_HASH = "fcdc178451cd234e63faefd38895c991"
-	BOT_TOKEN = "5594755883:AAF8cSNiYKLc659A-a6aqWFCM2fayFPtkD8"
-	BOT_USERNAME = "asumoviesharebot"
+	BOT_TOKEN = "5852899105:AAGyPXuplUXC6ir5iFOYIO9HwT4oYTYB7FQ"
+	BOT_USERNAME = "asufileslockerbot"
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001188070894"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "880087645"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Erichdaniken:Erichdaniken@cluster0.vhu3d.mongodb.net/?retryWrites=true&w=majority")
