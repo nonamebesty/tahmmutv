a4336c01cb1ffff08d92ce077b511d29d7d3544c
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-02-15 15:57:52 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -35,4 +35,9 @@ async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     #await reply_forward(message=sent_message, file_id=file_id)
     await asyncio.sleep(30)
-    await message.delete()
+    #await message.delete()
+await client.delete_messages(
+    chat_id=user_id,
+    message_ids=file_id
+)
+
