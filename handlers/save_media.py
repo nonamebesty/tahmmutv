2950ce76f05fac27ac10a68b4472e9892f1f5977
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-06-21 23:04:20 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -50,12 +50,19 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             ]])
         )
         share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=JAsuran_{str_to_b64(str(SaveMessage.id))}"
+        #Asuran
+        # get media type
+        media = message.document or message.video or message.audio or message.photo
+        # get file name
+        file_name = media.file_name if media.file_name else ""
+        # get caption (if any)
+        caption = message.caption if media.file_name else ""
         await editable.edit(
-            f"**Link:** {share_link}",
+            f"**{caption}\n\nLink:** {share_link}",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
                  [InlineKeyboardButton("Bots Channel", url="https://telegram.me/JAsuranBots"),
-                  InlineKeyboardButton("Support Group", url="https://telegram.me/AsuranMoviefinder")]]
+                  InlineKeyboardButton("Support Group", url="https://telegram.me/AsuranMoviefinders")]]
             ),
             disable_web_page_preview=True
         )
@@ -88,7 +95,7 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
             disable_web_page_preview=True)
         share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=JAsuran_{str_to_b64(file_er_id)}"
         await editable.edit(
-            f"**Link:** {share_link}",
+            f"**{caption}\n\nLink:** {share_link}",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
                  [InlineKeyboardButton("Bots Channel", url="https://telegram.me/JAsuranBots"),
