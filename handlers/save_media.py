77cb71aa215dbde81c3cc046d60999bd2c18b100
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-08-18 00:21:23 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -92,12 +92,12 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         # get file name
         file_name = media.file_name if media.file_name else ""
         # get file size
-        file_size = round(media.file_size/1024/1024, 3)
+        file_size = round(media.file_size/(1024*1024), 0)
         # get caption (if any)
         caption = message.caption if media.file_name else ""
         share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=JAsuran_{str_to_b64(file_er_id)}"
         await editable.edit(
-            f"**{caption}\n\nLink:** {share_link} - {file_size}",
+            f"**{caption} - {file_size}MB\n\nLink:** {share_link}",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
                  [InlineKeyboardButton("Bots Channel", url="https://telegram.me/JAsuranBots"),
