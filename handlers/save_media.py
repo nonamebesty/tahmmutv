2f64f96a2d2ebf4111abe9dcdfd730a8b070af50
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-08-26 08:34:36 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -64,14 +64,14 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
                 InlineKeyboardButton("Delete Batch", callback_data="closeMessage")
             ]])
         )
-        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=JAsuran_{str_to_b64(str(SaveMessage.id))}"
+        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=AsBots_{str_to_b64(str(SaveMessage.id))}"
 
         await editable.edit(
             f"**Link:** {share_link}",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://telegram.me/JAsuranBots"),
-                  InlineKeyboardButton("Support Group", url="https://telegram.me/AsuranMoviefinders")]]
+                 [InlineKeyboardButton("Bots Channel", url="https://telegram.me/AS_botzz"),
+                  InlineKeyboardButton("Support Group", url="https://telegram.me/asumoviefinderss")]]
             ),
             disable_web_page_preview=True
         )
@@ -114,11 +114,11 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         caption = message.caption if media.file_name else ""
         share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=JAsuran_{str_to_b64(file_er_id)}"
         await editable.edit(
-            f"**{caption} - {file_size}\n\nLink:** {share_link}",
+            f"**{file_size} - {file_name}\n\n   {share_link}**",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://telegram.me/JAsuranBots"),
-                  InlineKeyboardButton("Support Group", url="https://telegram.me/AsuranMoviefinder")]]
+                 [InlineKeyboardButton("Bots Channel", url="https://telegram.me/AS_botzz"),
+                  InlineKeyboardButton("Support Group", url="https://telegram.me/asumoviefinders")]]
             ),
             disable_web_page_preview=True
         )
