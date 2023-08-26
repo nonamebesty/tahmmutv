6dcdd71706006017806f63965698a71d49f52167
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-08-26 14:15:07 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ import os
 class Config(object):
 	API_ID = 1923471
 	API_HASH = "fcdc178451cd234e63faefd38895c991"
-	BOT_TOKEN = "6183267069:6183267069:AAHkuidgffjT5WndTpsSuOr7onhw5ybf3nc"
+	BOT_TOKEN = "6183267069:AAEO7w-CUF-ItP-PnNukVu_eTxXHesGIJPg"
 	BOT_USERNAME = "ASFileStoreBOt"
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001814192750"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "880087645"))
