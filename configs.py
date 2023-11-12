9dc3d2f5ba3d9c9c5bdef44fc0c261bcc7daab95
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-11-12 08:30:16 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,8 +7,8 @@ import os
 class Config(object):
 	API_ID = 1923471
 	API_HASH = "fcdc178451cd234e63faefd38895c991"
-	BOT_TOKEN = "6465614864:AAG7xiM7HMbJaToypba7wrh8JbMj4SpKIAk"
-	BOT_USERNAME = "akmfilestorebot"
+	BOT_TOKEN = "6829554995:AAHl1pFFifOYyicaeeAHifWQ9FMzwGQainU"
+	BOT_USERNAME = "akmfilestoresbot"
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001814192750"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "880087645"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://leecher:leecher@cluster0.606mkpi.mongodb.net/?retryWrites=true&w=majority")
