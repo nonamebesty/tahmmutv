c26c5450248bd836bc4e336fb2ef52462e3efc37
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-06-08 19:55:49 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -21,7 +21,7 @@ async def reply_forward(message: Message, file_id: int):
             f"**Kindly Subscribe and Support My Youtube Channel: https://www.youtube.com/@JAsuranvideos**\n\n"
             f"**Files will be Deleted After 15 min**\n\n"
             f"**__To Retrive the Stored File, just again open the link!__**\n\n"
-            f"**{caption}\n\nLink:** https://akmfilestorrbot.jasurun.workers.dev?start=Shakthimaan_{str_to_b64(str(file_id))}",
+            f"**{caption}\n\nLink:** https://redirect.nonamebesty.workers.dev?start=Shakthimaan_{str_to_b64(str(file_id))}",
             disable_web_page_preview=True, quote=True)
     except FloodWait as e:
         await asyncio.sleep(e.value)
