0fd07282a2176e8fd9da54e1e2bda304b02c1872
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-06-11 23:09:45 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -1,154 +1,58 @@
-#Jai (c) @JAsuran
-
 import asyncio
-from configs import Config
-from pyrogram import Client
-from pyrogram.types import (
-    Message,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton
-)
-from pyrogram.errors import FloodWait
-from handlers.helpers import str_to_b64
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils import executor
+from pyrogram import Client, filters
+import config
 
-def humanbytes(size):
-    # https://stackoverflow.com/a/49361727/4723940
-    # 2**10 = 1024
-    if not size:
-        return ""
-    power = 2**10
-    n = 0
-    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
-    while size > power:
-        size /= power
-        n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+API_TOKEN = 'YOUR_BOT_TOKEN'
 
-def human_size(bytes, units=[' bytes','KB','MB','GB','TB', 'PB', 'EB']):
-    """ Returns a human readable string representation of bytes """
-    return str(bytes) + units[0] if int(bytes) < 1024 else human_size(int(bytes)>>10, units[1:])
-    
-async def forward_to_channel(bot: Client, message: Message, editable: Message):
-    try:
-        __SENT = await message.forward(Config.DB_CHANNEL)
-        return __SENT
-    except FloodWait as sl:
-        if sl.value > 45:
-            await asyncio.sleep(sl.value)
-            await bot.send_message(
-                chat_id=int(Config.LOG_CHANNEL),
-                text=f"#FloodWait:\nGot FloodWait of `{str(sl.value)}s` from `{str(editable.chat.id)}` !!",
-                disable_web_page_preview=True,
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [InlineKeyboardButton("Ban User", callback_data=f"ban_user_{str(editable.chat.id)}")]
-                    ]
-                )
-            )
-        return await forward_to_channel(bot, message, editable)
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
-async def save_batch_media_in_channel(bot: Client, editable: Message, message_ids: list):
-    try:
-        message_ids_str = ""
-        for message in (await bot.get_messages(chat_id=editable.chat.id, message_ids=message_ids)):
-            sent_message = await forward_to_channel(bot, message, editable)
-            if sent_message is None:
-                continue
-            message_ids_str += f"{str(sent_message.id)} "
-            await asyncio.sleep(2)
-        SaveMessage = await bot.send_message(
-            chat_id=Config.DB_CHANNEL,
-            text=message_ids_str,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton("Delete Batch", callback_data="closeMessage")
-            ]])
-        )
-        share_link = f"https://redirect.nonamebesty.workers.dev?start=Shakthimaan_{str_to_b64(str(SaveMessage.id))}"
+class Form(StatesGroup):
+    waiting_for_filename = State()
 
-        await editable.edit(
-            f"**Link:** {share_link}",
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://telegram.me/AS_botzz"),
-                  InlineKeyboardButton("Support Group", url="https://telegram.me/moviekoodu")]]
-            ),
-            disable_web_page_preview=True
-        )
-        await bot.send_message(
-            chat_id=int(Config.LOG_CHANNEL),
-            text=f"#BATCH_SAVE:\n\n[{editable.reply_to_message.from_user.first_name}](tg://user?id={editable.reply_to_message.from_user.id}) Got Batch Link!",
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Open Link", url=share_link)]])
-        )
-    except Exception as err:
-        await editable.edit(f"Something Went Wrong!\n\n**Error:** `{err}`")
-        await bot.send_message(
-            chat_id=int(Config.LOG_CHANNEL),
-            text=f"#ERROR_TRACEBACK:\nGot Error from `{str(editable.chat.id)}` !!\n\n**Traceback:** `{err}`",
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton("Ban User", callback_data=f"ban_user_{str(editable.chat.id)}")]
-                ]
-            )
-        )
+@dp.message_handler(commands='start')
+async def cmd_start(message: types.Message):
+    await message.reply_text(
+        text="**Choose an option from below:**",
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("Save in Batch", callback_data="addToBatchTrue")],
+            [InlineKeyboardButton("Single Link", callback_data="addToBatchFalse")]
+        ]),
+        quote=True,
+        disable_web_page_preview=True
+    )
 
+@dp.callback_query_handler(lambda c: c.data == 'addToBatchTrue')
+async def process_callback_save_in_batch(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, "Please send the filename:")
+    await Form.waiting_for_filename.set()
 
-async def save_media_in_channel(bot: Client, editable: Message, message: Message):
-    try:
-        forwarded_msg = await message.forward(Config.DB_CHANNEL)
-        file_er_id = str(forwarded_msg.id)
-        await forwarded_msg.reply_text(
-            f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
-            disable_web_page_preview=True)
-        #Asuran
-        # get media type
-        media = message.document or message.video or message.audio or message.photo
-        # get file name
-        file_name = media.file_name if media.file_name else ""
-        # get file size
-        #file_size = round(media.file_size/(1024*1024), 1)
-        file_size = humanbytes(media.file_size)
-        # get caption (if any)
-        caption = message.caption if media.file_name else ""
-        share_link = f"https://redirect.nonamebesty.workers.dev?start=Shakthimaan_{str_to_b64(file_er_id)}"
-        await editable.edit(
-            f"**{caption} - {file_size}\n\n{share_link}**",
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://telegram.me/AS_botzz"),
-                  InlineKeyboardButton("Support Group", url="https://telegram.me/moviekoodu")]]
-            ),
-            disable_web_page_preview=True
-        )
-    except FloodWait as sl:
-        if sl.value > 45:
-            print(f"Sleep of {sl.value}s caused by FloodWait ...")
-            await asyncio.sleep(sl.value)
-            await bot.send_message(
-                chat_id=int(Config.LOG_CHANNEL),
-                text="#FloodWait:\n"
-                     f"Got FloodWait of `{str(sl.value)}s` from `{str(editable.chat.id)}` !!",
-                disable_web_page_preview=True,
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [InlineKeyboardButton("Ban User", callback_data=f"ban_user_{str(editable.chat.id)}")]
-                    ]
-                )
-            )
-        await save_media_in_channel(bot, editable, message)
-    except Exception as err:
-        await editable.edit(f"Something Went Wrong!\n\n**Error:** `{err}`")
-        await bot.send_message(
-            chat_id=int(Config.LOG_CHANNEL),
-            text="#ERROR_TRACEBACK:\n"
-                 f"Got Error from `{str(editable.chat.id)}` !!\n\n"
-                 f"**Traceback:** `{err}`",
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton("Ban User", callback_data=f"ban_user_{str(editable.chat.id)}")]
-                ]
-            )
-        )
+@dp.message_handler(state=Form.waiting_for_filename, content_types=types.ContentTypes.TEXT)
+async def process_filename(message: types.Message, state: FSMContext):
+    filename = message.text
+    await state.update_data(filename=filename)
+    await state.finish()
+    # Generate the output link with the filename
+    output_link = f"https://redirect.nonamebesty.workers.dev?start=Shakthimaan_{filename}"
+
+    await message.reply_text(
+        f"Filename: {filename}\nOutput link: {output_link}",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Open Link", url=output_link)],
+             [InlineKeyboardButton("Bots Channel", url="https://telegram.me/AS_botzz"),
+              InlineKeyboardButton("Support Group", url="https://telegram.me/moviekoodu")]]
+        ),
+        disable_web_page_preview=True
+    )
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
