d2ac2bcac7d59a9578d086ef0284bc51a1b1d5fa
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-06-11 13:02:34 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -1,5 +1,3 @@
-# (c) @JAsuran
-
 import asyncio
 from configs import Config
 from pyrogram import Client, filters
@@ -16,8 +14,6 @@ from handlers.helpers import str_to_b64
 user_filename_input = {}
 
 def humanbytes(size):
-    # https://stackoverflow.com/a/49361727/4723940
-    # 2**10 = 1024
     if not size:
         return ""
     power = 2**10
@@ -28,10 +24,6 @@ def humanbytes(size):
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
-def human_size(bytes, units=[' bytes','KB','MB','GB','TB', 'PB', 'EB']):
-    """ Returns a human readable string representation of bytes """
-    return str(bytes) + units[0] if int(bytes) < 1024 else human_size(int(bytes)>>10, units[1:])
-    
 async def forward_to_channel(bot: Client, message: Message, editable: Message):
     try:
         __SENT = await message.forward(Config.DB_CHANNEL)
@@ -53,13 +45,71 @@ async def forward_to_channel(bot: Client, message: Message, editable: Message):
 
 async def save_batch_media_in_channel(bot: Client, editable: Message, message_ids: list):
     try:
-        message_ids_str = ""
+        forwarded_messages = []
         for message in (await bot.get_messages(chat_id=editable.chat.id, message_ids=message_ids)):
             sent_message = await forward_to_channel(bot, message, editable)
             if sent_message is None:
                 continue
-            message_ids_str += f"{str(sent_message.id)} "
+            forwarded_messages.append(sent_message)
             await asyncio.sleep(2)
+
+        user_id = editable.reply_to_message.from_user.id
+        user_filename_input[user_id] = {"editable": editable, "messages": forwarded_messages, "filenames": []}
+
+        await editable.edit("Please provide filenames for each file, separated by commas:")
+    except Exception as err:
+        await editable.edit(f"Something Went Wrong!\n\n**Error:** `{err}`")
+        await bot.send_message(
+            chat_id=int(Config.LOG_CHANNEL),
+            text=f"#ERROR_TRACEBACK:\nGot Error from `{str(editable.chat.id)}` !!\n\n**Traceback:** `{err}`",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton("Ban User", callback_data=f"ban_user_{str(editable.chat.id)}")]
+                ]
+            )
+        )
+
+@Client.on_message(filters.private & filters.text)
+async def receive_filename(bot: Client, message: Message):
+    user_id = message.from_user.id
+
+    if user_id not in user_filename_input:
+        return
+
+    filenames = message.text.split(',')
+    data = user_filename_input.pop(user_id)
+    editable = data["editable"]
+    forwarded_messages = data["messages"]
+
+    if len(filenames) != len(forwarded_messages):
+        await editable.edit("The number of filenames provided does not match the number of files. Please try again.")
+        user_filename_input[user_id] = {"editable": editable, "messages": forwarded_messages, "filenames": []}
+        return
+
+    try:
+        message_ids_str = ""
+        for idx, forwarded_msg in enumerate(forwarded_messages):
+            file_er_id = str(forwarded_msg.id)
+            filename = filenames[idx].strip()
+            await forwarded_msg.reply_text(
+                f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
+                disable_web_page_preview=True)
+            media = forwarded_msg.document or forwarded_msg.video or forwarded_msg.audio or forwarded_msg.photo
+            file_size = humanbytes(media.file_size)
+            caption = forwarded_msg.caption if media.file_name else ""
+            share_link = f"https://redirect.nonamebesty.workers.dev?start=Shakthimaan_{str_to_b64(file_er_id)}_{filename}"
+            message_ids_str += f"{str(forwarded_msg.id)} "
+            await editable.edit(
+                f"<blockquote>**{caption} - {file_size}\n\nFilename: {filename}\n\n{share_link}**</blockquote>",
+                reply_markup=InlineKeyboardMarkup(
+                    [[InlineKeyboardButton("Open Link", url=share_link)],
+                     [InlineKeyboardButton("Bots Channel", url="https://telegram.me/AS_botzz"),
+                      InlineKeyboardButton("Support Group", url="https://telegram.me/moviekoodu")]]
+                ),
+                disable_web_page_preview=True
+            )
+
         SaveMessage = await bot.send_message(
             chat_id=Config.DB_CHANNEL,
             text=message_ids_str,
@@ -85,62 +135,6 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Open Link", url=share_link)]])
         )
-    except Exception as err:
-        await editable.edit(f"Something Went Wrong!\n\n**Error:** `{err}`")
-        await bot.send_message(
-            chat_id=int(Config.LOG_CHANNEL),
-            text=f"#ERROR_TRACEBACK:\nGot Error from `{str(editable.chat.id)}` !!\n\n**Traceback:** `{err}`",
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton("Ban User", callback_data=f"ban_user_{str(editable.chat.id)}")]
-                ]
-            )
-        )
-
-
-async def save_media_in_channel(bot: Client, editable: Message, message: Message):
-    user_id = message.from_user.id
-    user_filename_input[user_id] = {"editable": editable, "message": message}
-
-    await editable.edit("Please provide a filename for this file:")
-    return
-
-@Client.on_message(filters.private & filters.text)
-async def receive_filename(bot: Client, message: Message):
-    user_id = message.from_user.id
-
-    if user_id not in user_filename_input:
-        return
-
-    filename = message.text
-    data = user_filename_input.pop(user_id)
-    editable = data["editable"]
-    original_message = data["message"]
-
-    try:
-        forwarded_msg = await original_message.forward(Config.DB_CHANNEL)
-        file_er_id = str(forwarded_msg.id)
-        await forwarded_msg.reply_text(
-            f"#PRIVATE_FILE:\n\n[{original_message.from_user.first_name}](tg://user?id={original_message.from_user.id}) Got File Link!",
-            disable_web_page_preview=True)
-        #Asuran
-        # get media type
-        media = original_message.document or original_message.video or original_message.audio or original_message.photo
-        # get file size
-        file_size = humanbytes(media.file_size)
-        # get caption (if any)
-        caption = original_message.caption if media.file_name else ""
-        share_link = f"https://redirect.nonamebesty.workers.dev?start=Shakthimaan_{str_to_b64(file_er_id)}_{filename}"
-        await editable.edit(
-            f"<blockquote>**{caption} - {file_size}\n\nFilename: {filename}\n\n{share_link}**</blockquote>",
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://telegram.me/AS_botzz"),
-                  InlineKeyboardButton("Support Group", url="https://telegram.me/moviekoodu")]]
-            ),
-            disable_web_page_preview=True
-        )
     except FloodWait as sl:
         if sl.value > 45:
             print(f"Sleep of {sl.value}s caused by FloodWait ...")
@@ -156,7 +150,7 @@ async def receive_filename(bot: Client, message: Message):
                     ]
                 )
             )
-        await save_media_in_channel(bot, editable, original_message)
+        await receive_filename(bot, message)
     except Exception as err:
         await editable.edit(f"Something Went Wrong!\n\n**Error:** `{err}`")
         await bot.send_message(
