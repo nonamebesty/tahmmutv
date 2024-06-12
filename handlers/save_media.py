108caa31a8e54dc8e7ec47fad97998212b5946a5
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-06-12 21:47:37 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -84,7 +84,7 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
         share_link = f"https://redirect.nonamebesty.workers.dev?start=Shakthimaan_{str_to_b64(str(SaveMessage.id))}"
         short_link = get_short(share_link)
         await editable.edit(
-            f"**🔻 Direct Telegram Files 🔻\n\n🔹 {short_link}\n⁉️ How to Download ▼\nhttps://t.me/demoshort/49**",
+            f"**🔻 Direct Telegram Files 🔻\n\n{short_link}\n\n⁉️ How to Download ▼\nhttps://t.me/demoshort/49**",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
                  [InlineKeyboardButton("Bots Channel", url="https://telegram.me/AS_botzz"),
