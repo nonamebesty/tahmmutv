def3cbc805de6d76e26e712fef70b26947a297dc
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-06-22 19:37:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config(object):
  API_ID = int(os.environ.get("API_ID"))
  API_HASH = os.environ.get("API_HASH")
  BOT_TOKEN = os.environ.get("BOT_TOKEN")
- BOT_USERNAME = os.environ.get("BOT_USERNAME)
+ BOT_USERNAME = os.environ.get("BOT_USERNAME")
  DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
  BOT_OWNER = int(os.environ.get("BOT_OWNER")
  DATABASE_URL = os.environ.get("DATABASE_URL")
